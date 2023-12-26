697ef697ded1b4eec6a5956f88937b174746e2f7
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2023-12-25 21:32:16 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="Meng" body="Meng is awesome" />
+      <Post author="Vivoe" body="Vivoe is fabulours" />
+    </main>
+  );
+}
+
+export default App;
