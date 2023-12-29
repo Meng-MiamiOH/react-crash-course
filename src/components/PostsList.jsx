ca9a95ed5f9d11439dfa29a8597fc8c1a00c5b0b
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2023-12-29 09:16:34 -0500

    Added loading posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
+  // Don't write it in this way: it will cause an infinite loop
+  // fetch("http://localhost:8080/posts")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     setPosts(data.posts);
+  //   });
+
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
@@ -25,16 +43,22 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 ? (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
-      ) : (
+      )}
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
